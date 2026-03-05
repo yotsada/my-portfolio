ab4ba0b0bf5b14d3c4bc1608cf5e178025deb3ed
IM05 <yotsada.p@gmail.com>
2026-03-05 14:02:49 +0700

    hand animetion fix_5

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const frames = [
 "assets/hand2.png",
 "assets/hand3.png",
 "assets/hand4.png",
-"assets/hand5.png",
-"assets/hand6.png"
+"assets/hand5.png"
 ]
 
 const img = document.getElementById("hand")
@@ -13,15 +12,22 @@ window.addEventListener("scroll", () => {
 
 const intro = document.querySelector(".intro")
 
-const scroll = window.scrollY
-const max = intro.offsetHeight - window.innerHeight
+const scrollTop = window.scrollY
+const maxScroll = intro.offsetHeight - window.innerHeight
 
-let progress = scroll / max
-
+let progress = scrollTop / maxScroll
 progress = Math.min(Math.max(progress,0),1)
 
-const frame = Math.floor(progress * (frames.length - 1))
+const frameIndex = Math.floor(progress * (frames.length-1))
 
-img.src = frames[frame]
+img.src = frames[frameIndex]
+
+/* ซ่อนมือหลัง animation */
+
+if(progress > 0.9){
+img.style.opacity = "0"
+}else{
+img.style.opacity = "1"
+}
 
 })
